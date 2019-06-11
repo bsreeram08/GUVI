6e8db0c39b594841f2e7f93bb8e6b67fe878f146
Sreeram Balamurugan <35658317+bsreeram08@users.noreply.github.com>
2019-06-11 13:59:58 +0530

    Create lastnewfile.cpp

---
+++ lastnewfile.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int m,n,temp;
+	cin>>m>>n;
+	map<int,int> arr;
+	for(int i=0;i<m;i++)
+	{
+	    cin>>temp;
+	    arr[temp]++;
+	}
+	bool flag=true;
+	for(int i=0;i<n;i++)
+	{
+	    cin>>temp;
+	    if(arr.find(temp)!=arr.end())
+	    {
+	        flag=false;
+	    }
+	}
+	if(flag)
+	{
+	    cout<<"YES";
+	}
+	else
+	{
+	    cout<<"NO";
+	}
+	return 0;
+}
