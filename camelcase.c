3a4deed7ca81451095664588ce92731aedaa8a75
Sreeram Balamurugan <35658317+bsreeram08@users.noreply.github.com>
2019-06-11 10:19:46 +0530

    updated

--- camelcase.c
+++ camelcase.c
@@ -5,6 +5,10 @@ int main() {
 	{
 	    if(isalpha(data[0]))
 	        data[0]=toupper(data[0]);
+	    for(int i=1;i<strlen(data);i++)
+	    {
+	        data[i]=tolower(data[i]);
+	    }
 	    printf("%s ",data);
 	}
 	printf("%c",8);
